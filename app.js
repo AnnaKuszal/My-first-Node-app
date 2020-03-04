06a413acb6711855d2aac1d7510d97df2f713191
Anna Kuszal <anna.kuszal@o2.pl>
2020-03-04 13:32:22 +0100

    Add data for app

---
+++ app.js
@@ -0,0 +1,11 @@
+const os = require('os');
+const fs = require('fs');
+
+console.log('Platform: ', os.platform());
+console.log('Arch: ', os.arch());
+
+const genders = ["K", "M"]; 
+const maleNames = ["Adrian", "Bartek", "Tomasz", "Marek", "Grzegorz", "Rafal", "Piotr", "Mariusz", "Maciej", "Jerzy"]; 
+const femaleNames = ["Ada", "Rita", "Agnieszka", "Lidia", "Paulina", "Marta", "Nina", "Róża", "Jolanta", "Jagoda"];
+const lastNames = ["Nowak", "Kowal", "Wydro", "Stepien", "Surowiec", "Sondej", "Gruza", "Moryto", "Smolik", "Ortyl"];
+
